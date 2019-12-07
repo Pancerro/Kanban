02c62650214d072cacbe9c70e052561abe19fd9b
Adrian Ławecki <pancerro@gmail.com>
2019-12-07 22:05:19 +0100

    login frontend #KB-14

--- src/app/dashboards/dashboards/dashboards.component.ts
+++ src/app/dashboards/dashboards/dashboards.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboards',
   templateUrl: './dashboards.component.html',
   styleUrls: ['./dashboards.component.css']
 })
-export class DashboardsComponent implements OnInit {
+export class DashboardsComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private auth:AuthService,
+    private router: Router,
+    ) { }
+  verifyEmail=this.auth.user.emailVerified;
+  logout(){
+    this.auth.logout().then(() => this.router.navigate(['/welcome-page']));
   }
 
 }
