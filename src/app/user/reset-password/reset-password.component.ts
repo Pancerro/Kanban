b5b84e30b619b26bf25243a1a6ce40b7a8201d2b
Adrian Ławecki <pancerro@gmail.com>
2019-12-14 13:46:02 +0100

    upgrade emaol verify #KB-1

--- src/app/user/reset-password/reset-password.component.ts
+++ src/app/user/reset-password/reset-password.component.ts
@@ -11,17 +11,22 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class ResetPasswordComponent{
 info:string;
 hide:boolean = true;
+emailVerify;
 mode = this.activatedActivated.snapshot.queryParams['mode'];
 code = this.route.snapshot.queryParams['oobCode'];
   constructor( public auth:AuthService,
     private afAuth: AngularFireAuth, 
     private router: Router,
     private route: ActivatedRoute,
-    private activatedActivated: ActivatedRoute) { }
+    private activatedActivated: ActivatedRoute) {}
+
+  emailVerificate(){
+    this.emailVerify=this.auth.getUser().emailVerified;
+    return this.emailVerify;
+  }
   resetUserPassword(updatePassword):void{
     if(this.matchingPasswords(updatePassword.value.reset.newPassword,updatePassword.value.reset.newRepeatPassword)){
-      this.afAuth.auth
-      .confirmPasswordReset(this.code,updatePassword.value.reset.newPassword)
+      this.auth.userResetPassword(this.code,updatePassword.value.reset.newPassword)
       .then(() => this.router.navigate(['welcome-page']))
     }
   }
